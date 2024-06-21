c8b645c0d4576c6d39871239a19a6c67fa0ddc23
Zheng Qiu <liqiu2003@gmail.com>
2024-06-21 19:16:44 -0400

    interface

--- database_app.py
+++ database_app.py
@@ -1,1 +1,37 @@
-import argparse
+import click
+import impl
+
+if __name__ == '__main__':
+    print("Connecting to database...")
+    con = impl.connect()
+    cur = con.cursor()
+    reset = click.confirm("Do you want to reset the database and repull data into tables?")
+    if reset:
+        impl.reset(cur)
+        con.commit()
+        print("Database reset.")
+        print("Pulling data into tables...")
+        impl.pull(cur)
+    
+    while True:
+        features = """
+        We support following features:
+        1. List neighborhoods where the number of events is greater than or equal to a specified threshold.
+        2. Count the types of outcome for all events.
+        3. Rank all communities in terms of the average price of vehicles that got stolen.
+        4. Check the latest events captured in the dataset.
+        5. Check insurance payments for vehicles that got stolen. 
+        6. Check police resposible for handled events.
+        Please select a feature to run (1-6) or type anything else to quit:
+        """
+        print(features)
+        feature = input()
+        if feature not in ['1', '2', '3', '4', '5', '6']:
+            break
+        input = None
+        if feature == '1':
+            input = int(input("Please enter the threshold: "))
+        output = impl.features(cur, int(feature), input)
+        print(output)
+        
+    con.close()
